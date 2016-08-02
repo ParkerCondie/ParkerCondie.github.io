b774d86233465286e2ed964e6c6096d814120031
Parker condie <jameserjimmer@gmail.com>
2016-08-02 14:32:21 +0000

    Updated vaporizer and sign-in

---
+++ Sign-in/sign.js
@@ -0,0 +1,6 @@
+function signUp() {
+   var user = document.getElementById("user").value;
+   var pass = document.getElementById("pass").value;
+   console.log (user + pass);
+   alert ("Your username is '" + user + "'and your password is '" + pass + "'.");
+}
